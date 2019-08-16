1ee50ca1ee13d9938ff9c97519b9e67d3c1dd0ec
chenyang1999 <34475230+chenyang1999@users.noreply.github.com>
2019-08-16 13:24:18 +0800

    默认

--- 代码技巧汇总/pytorch_psnr_ssim.py
+++ 代码技巧汇总/pytorch_psnr_ssim.py
@@ -4,19 +4,33 @@ per_image_psnr = 10 * torch.log10 (10 / per_image_mse_loss)
 tensor_average_psnr = torch.mean (per_image_psnr).item ()
 
 #SSIM
-from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
-# X: (N,3,H,W) a batch of RGB images with values ranging from 0 to 255.
-# Y: (N,3,H,W)  
-ssim_val = ssim( X, Y, data_range=255, size_average=False) # return (N,)
-ms_ssim_val = ms_ssim( X, Y, data_range=255, size_average=False ) #(N,)
+import pytorch_ssim
+import torch
+from torch.autograd import Variable
 
-# or set 'size_average=True' to get a scalar value as loss.
-ssim_loss = ssim( X, Y, data_range=255, size_average=True) # return a scalar value
-ms_ssim_loss = ms_ssim( X, Y, data_range=255, size_average=True )
+img1 = Variable(torch.rand(1, 1, 256, 256))
+img2 = Variable(torch.rand(1, 1, 256, 256))
 
-# or reuse windows with SSIM & MS_SSIM. 
-ssim_module = SSIM(win_size=11, win_sigma=1.5, data_range=255, size_average=True, channel=3)
-ms_ssim_module = MS_SSIM(win_size=11, win_sigma=1.5, data_range=255, size_average=True, channel=3)
+if torch.cuda.is_available():
+	img1 = img1.cuda()
+	img2 = img2.cuda()
 
-ssim_loss = ssim_module(X, Y)
-ms_ssim_loss = ms_ssim_module(X, Y)
+print(pytorch_ssim.ssim(img1, img2))
+
+ssim_loss = pytorch_ssim.SSIM(window_size = 11)
+
+print(ssim_loss(img1, img2))
+
+#MSSSIM
+import pytorch_ssim
+import torch
+from torch.autograd import Variable
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+m = pytorch_msssim.MSSSIM()
+
+img1 = torch.rand(1, 1, 256, 256)
+img2 = torch.rand(1, 1, 256, 256)
+
+print(pytorch_msssim.msssim(img1, img2))
+print(m(img1, img2)))
